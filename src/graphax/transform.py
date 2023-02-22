7f0a89bed1e3884c125b1661a2d4125713a713f5
Jamie Lohoff <j.grieser@fz-juelich.de>
2023-02-22 13:45:55 +0100

    Started adding symmetry transformations to the computational graph.

---
+++ src/graphax/transform.py
@@ -0,0 +1,11 @@
+"""
+This file contains methods that provide symmetry operations that can be
+safely used on computational graphs for data augumentation.
+"""
+
+import jax
+import jax.numpy as jnp
+
+import numpy as np
+
+
